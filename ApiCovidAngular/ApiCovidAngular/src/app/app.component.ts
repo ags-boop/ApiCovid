f562215716442f24414137fd9384730f8a258c49
Agustin Testa <testaagustin8@gmail.com>
2021-04-03 15:55:48 -0300

    Get data for country

--- ApiCovidAngular/ApiCovidAngular/src/app/app.component.ts
+++ ApiCovidAngular/ApiCovidAngular/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http: HttpClient, private router: ActivatedRoute) { }
+  readonly ROOTURL = `https://api.covid19api.com/dayone/country/Argentina/status/confirmed`
+  GetJson() {
+    fetch(this.ROOTURL)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
+  getInput() {
+    let foodName = (document.getElementById("countryname") as HTMLInputElement).value;   
+    console.log(foodName)
+    fetch(`https://api.covid19api.com/dayone/country/${foodName}/status/confirmed`)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
   title = 'ApiCovidAngular';
 }
